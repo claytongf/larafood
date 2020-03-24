b398e1ac1213a18329b37fcb1dd257225f79c8f8
Clayton Figueiredo <claytongf@gmail.com>
2020-03-24 15:51:09 -0300

    First Commit

---
+++ app/Models/Plan.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Plan extends Model
+{
+    protected $fillable = ['name', 'url', 'price', 'description'];
+
+    public function search($filter = null){
+        return $this->where('name', 'LIKE', "%{$filter}%")
+            ->orWhere('description', 'LIKE', "%{$filter}%")
+            ->paginate(1);
+    }
+
+    public function details(){
+        return $this->hasMany(DetailPlan::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->group(function (){
+    Route::get('plans', 'PlanController@index')->name('plans.index');
+    Route::get('plans/create', 'PlanController@create')->name('plans.create');
+    Route::post('plans', 'PlanController@store')->name('plans.store');
+    Route::get('plans/{url}', 'PlanController@show')->name('plans.show');
+    Route::delete('plans/{url}', 'PlanController@destroy')->name('plans.destroy');
+    Route::any('plans/search', 'PlanController@search')->name('plans.search');
+    Route::get('plans/{url}/edit', 'PlanController@edit')->name('plans.edit');
+    Route::put('plans/{url}', 'PlanController@update')->name('plans.update');
+    Route::get('/', 'PlanController@index')->name('admin.index');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
