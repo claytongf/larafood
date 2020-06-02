86278c66aa949e93dc9a2e23e019385c861e652a
Clayton Figueiredo <claytongf@gmail.com>
2020-06-02 10:39:34 -0300

    Generate QrCode Table

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -11,48 +11,56 @@ class TableController extends Controller
 {
     private $repository;
 
-    public function __construct(Table $table){
+    public function __construct(Table $table)
+    {
         $this->repository = $table;
     }
 
-    public function index(){
+    public function index()
+    {
         $tables = $this->repository->latest()->paginate();
         return view('admin.pages.tables.index', compact('tables'));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('admin.pages.tables.create');
     }
 
-    public function store(StoreUpdateTableRequest $request){
+    public function store(StoreUpdateTableRequest $request)
+    {
         $this->repository->create($request->all());
         return redirect()->route('tables.index');
     }
 
-    public function edit($id){
-        if(!$table = $this->repository->find($id)){
+    public function edit($id)
+    {
+        if (!$table = $this->repository->find($id)) {
             return redirect()->back();
         }
         return view('admin.pages.tables.edit', compact('table'));
     }
 
-    public function update(StoreUpdateTableRequest $request, $id){
-        if(!$table = $this->repository->find($id)){
+    public function update(StoreUpdateTableRequest $request, $id)
+    {
+        if (!$table = $this->repository->find($id)) {
             return redirect()->back();
         }
         $table->update($request->all());
         return redirect()->route('tables.index');
     }
 
-    public function show($id){
-        if(!$table = $this->repository->find($id)){
+    public function show($id)
+    {
+        if (!$table = $this->repository->find($id)) {
             return redirect()->back();
         }
         return view('admin.pages.tables.show', compact('table'));
     }
 
-    public function destroy($id){
-        if(!$table = $this->repository->find($id)){
+    public function destroy($id)
+    {
+        if (!$table = $this->repository->find($id)) {
             return redirect()->back();
         }
         $table->delete();
@@ -76,5 +84,24 @@ class TableController extends Controller
         })->latest()->paginate();
 
         return view('admin.pages.tables.index', compact('tables', 'filters'));
+    }
+
+    /**
+     * Generate QrCode Table
+     *
+     * @param  int $identify
+     * @return \Illuminate\Http\Response
+     */
+    public function qrcode($identify)
+    {
+        if (!$table = $this->repository->where('identify', $identify)->first()) {
+            return redirect()->back();
+        }
+
+        $tenant = auth()->user()->tenant;
+
+        $uri = env('URI_CLIENT') . "/{$tenant->uuid}/{$table->uuid}";
+
+        return view('admin.pages.tables.qrcode', compact('uri'));
     }
 }
--- resources/views/admin/pages/tables/index.blade.php
+++ resources/views/admin/pages/tables/index.blade.php
@@ -36,6 +36,7 @@
                             <td>{{ $table->identify }}</td>
                             <td>{{ $table->description }}</td>
                         <td>
+                            <a href="{{ route('tables.qrcode', $table->identify) }}" class="btn btn-default" target="_blank"><i class="fas fa-qrcode"></i></a>
                             <a href="{{ route('tables.edit', $table->id) }}" class="btn btn-info">Editar</a>
                             <a href="{{ route('tables.show', $table->id) }}" class="btn btn-warning">Ver</a>
                         </td>
